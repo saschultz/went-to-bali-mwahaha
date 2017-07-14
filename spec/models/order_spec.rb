7b89e0dc7102b385c9fa0540f114bd2aa3fc8e07
Sara Schultz <saschultz95@gmail.com>
2017-07-14 10:26:36 -0700

    pass spec for callback method before save in order.rb

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 describe Order do
   it { should have_many :order_items }
   it { should belong_to :user }
-  
+  it { is_expected.to callback(:calculate_total).before(:save)}
 end
