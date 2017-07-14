6ad8b179d29f05bb1c75f948fcaf87f2f3d66f6c
Sara Schultz <saschultz95@gmail.com>
2017-07-14 10:28:01 -0700

    add notes in order_spec.rb for future tests

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -4,4 +4,10 @@ describe Order do
   it { should have_many :order_items }
   it { should belong_to :user }
   it { is_expected.to callback(:calculate_total).before(:save)}
+
+  ###### implement factory girl and test calculate_total method ######
+  ####################################################################
+  
+  ###### implement factory girl and test finalize method ######
+  ####################################################################
 end
