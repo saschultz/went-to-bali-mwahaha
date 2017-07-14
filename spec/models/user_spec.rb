f1f95dd16dec31abf6cfbeb6f6c546c8cbb79bf4
Sara Schultz <saschultz95@gmail.com>
2017-07-14 10:17:53 -0700

    pass spec for relating products and order items in product.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,8 @@ describe User do
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
   it { should validate_presence_of :name }
+
+  ###### implement factory girl and test previous_orders method ######
+  ####################################################################
+  
 end
