1f4090784d80c912b904f3fd09ed1dcd8999dc13
Sara Schultz <saschultz95@gmail.com>
2017-07-14 10:06:10 -0700

    pass sepc for validating presence of email in user.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe User do
-  it { should have_many :orders }
   it { should have_secure_password }
+  it { should have_many :orders }
+  it { should validate_presence_of :email }
+
 end
