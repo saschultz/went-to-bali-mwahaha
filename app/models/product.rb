bb5855124ad5f296d935144f376c35cccb544c52
Sara Schultz <saschultz95@gmail.com>
2017-07-15 15:31:53 -0700

    pass spec for validating presence of image in product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
   has_many :order_items
-  validates(:name, :description, :price, presence: true)
+  validates(:name, :description, :price, :image, presence: true)
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,4 +5,5 @@ describe Product do
   it { should validate_presence_of :name }
   it { should validate_presence_of :description }
   it { should validate_presence_of :price }
+  it { should validate_presence_of :image }
 end
