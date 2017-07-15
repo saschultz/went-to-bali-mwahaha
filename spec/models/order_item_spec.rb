31430a86bf8232a4dfc43fd34772463b0bc8f32d
Sara Schultz <saschultz95@gmail.com>
2017-07-15 15:11:06 -0700

    pass spec for quantity validation in order_item.rb

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 describe OrderItem do
   it { should belong_to :order }
   it { should belong_to :product }
+  it { should validate_numericality_of(:quantity)
+      .is_greater_than_or_equal_to(0) }
 end
